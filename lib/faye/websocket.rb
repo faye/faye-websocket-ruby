f4ea6b3557136d6808b9e120e83842d2f0458348
James Coglan <jcoglan@gmail.com>
2011-11-27 02:21:09 +0000

    Include links to relevent API specs.

--- lib/faye/websocket.rb
+++ lib/faye/websocket.rb
@@ -1,3 +1,12 @@
+# API and protocol references:
+# 
+# * http://dev.w3.org/html5/websockets/
+# * http://dvcs.w3.org/hg/domcore/raw-file/tip/Overview.html#interface-eventtarget
+# * http://dvcs.w3.org/hg/domcore/raw-file/tip/Overview.html#interface-event
+# * http://tools.ietf.org/html/draft-hixie-thewebsocketprotocol-75
+# * http://tools.ietf.org/html/draft-hixie-thewebsocketprotocol-76
+# * http://tools.ietf.org/html/draft-ietf-hybi-thewebsocketprotocol-17
+
 require 'base64'
 require 'digest/md5'
 require 'digest/sha1'
