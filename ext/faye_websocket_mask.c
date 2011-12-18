2f9a3180645c4cd46b8c3f101bdb6f2c42bb253a
James Coglan <jcoglan@gmail.com>
2011-12-18 12:59:17 +0000

    Don't use a loop to build the mask array.

--- ext/faye_websocket_mask.c
+++ ext/faye_websocket_mask.c
@@ -16,12 +16,14 @@ void Init_faye_websocket_mask() {
 
 VALUE method_faye_websocket_mask(VALUE self, VALUE payload, VALUE mask) {
   int n = RARRAY_LEN(payload), i, p, m;
-  int mask_array[4];
   VALUE unmasked = rb_ary_new2(n);
   
-  for (i = 0; i < 4; i++) {
-    mask_array[i] = NUM2INT(rb_ary_entry(mask, i));
-  }
+  int mask_array[] = {
+    NUM2INT(rb_ary_entry(mask, 0)),
+    NUM2INT(rb_ary_entry(mask, 1)),
+    NUM2INT(rb_ary_entry(mask, 2)),
+    NUM2INT(rb_ary_entry(mask, 3))
+  };
   
   for (i = 0; i < n; i++) {
     p = NUM2INT(rb_ary_entry(payload, i));
