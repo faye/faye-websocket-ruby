5a0c1097b6cb80c1fedac626f517dea89d3ec9ef
James Coglan <jcoglan@gmail.com>
2013-07-04 21:33:24 +0100

    Remove event buffering in favour of starting the driver asyncronously, so that onopen can be registered on server sockets.

--- lib/faye/websocket/api/event_target.rb
+++ lib/faye/websocket/api/event_target.rb
@@ -2,26 +2,10 @@ module Faye::WebSocket::API
   module EventTarget
 
     include ::WebSocket::Driver::EventEmitter
-    events = %w[open message error close]
-
-    events.each do |event_type|
-      define_method "on#{event_type}=" do |handler|
-        EventMachine.next_tick do
-          flush(event_type, &handler)
-          instance_variable_set("@on#{event_type}", handler)
-        end
-      end
-    end
+    attr_accessor :onopen, :onmessage, :onerror, :onclose
 
     def add_event_listener(event_type, listener, use_capture = false)
       add_listener(event_type, &listener)
-    end
-
-    def add_listener(event_type, &listener)
-      EventMachine.next_tick do
-        flush(event_type, &listener)
-        super(event_type, &listener)
-      end
     end
 
     def remove_event_listener(event_type, listener, use_capture = false)
@@ -33,23 +17,9 @@ module Faye::WebSocket::API
       event.event_phase = Event::AT_TARGET
 
       callback = instance_variable_get("@on#{ event.type }")
-      count    = listener_count(event.type)
-
-      unless callback or count > 0
-        @buffers ||= Hash.new { |k,v| k[v] = [] }
-        @buffers[event.type].push(event)
-      end
 
       callback.call(event) if callback
       emit(event.type, event)
-    end
-
-  private
-
-    def flush(event_type, &callback)
-      if buffer = @buffers && @buffers.delete(event_type.to_s)
-        buffer.each { |event| callback.call(event) }
-      end
     end
 
   end
