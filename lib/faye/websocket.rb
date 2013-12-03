5da07b76aa7db6d44384e536ae8f12b7d2fce6f0
James Coglan <jcoglan@gmail.com>
2013-12-03 12:35:43 +0000

    Remove some extraneous spaces.

--- lib/faye/websocket.rb
+++ lib/faye/websocket.rb
@@ -64,10 +64,10 @@ module Faye
     def initialize(env, protocols = nil, options = {})
       WebSocket.ensure_reactor_running
 
-      @env     = env
-      @url     = WebSocket.determine_url(@env)
-      @driver  = ::WebSocket::Driver.rack(self, :max_length => options[:max_length], :protocols => protocols)
-      @stream  = Stream.new(self)
+      @env    = env
+      @url    = WebSocket.determine_url(@env)
+      @driver = ::WebSocket::Driver.rack(self, :max_length => options[:max_length], :protocols => protocols)
+      @stream = Stream.new(self)
 
       if callback = @env['async.callback']
         callback.call([101, {}, @stream])
